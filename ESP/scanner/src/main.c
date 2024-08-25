857a2b3d2e6c171ff7d714e1d7e410f1302878f7
bigboingy <karu.home@gmail.com>
2024-08-25 15:55:35 +1000

    moved queue to task notification

--- ESP/scanner/src/main.c
+++ ESP/scanner/src/main.c
@@ -22,26 +22,27 @@
 #define UART_PORT_NO 2 // Port 0 is used for USB by default
 #define BAUD 115200 // Used for lidar and USB
 #define LIDAR_BUF_SIZE 1024 // DOES IT NEED TO BE THIS BIG?
-static QueueHandle_t uart2_queue; // Queue handle for interrupt, which adds an item to the queue on data received
 
 // IMU
 #define SCL 25 // D2. 4 (D12) Will be used in final PCB, but will ruin debugging now!
 #define SDA 26 // D3. 12 (D13) Will be used in final PCB
 #define IMU_BUF_SIZE 22
-static QueueHandle_t imu_queue;
 uint8_t imuBank; // Track current imu bank
 uint8_t const ACC_SCALE = 1;
 uint8_t const GYRO_SCALE = 2;
 uint16_t const OFFS_VALS = 100u;
 
-
 // LED
 #define LED 2
 static uint8_t led_state = 0;
 
+// Tasks
+static QueueHandle_t uart2_queue; // Queue handle for interrupt, which adds an item to the queue on data received
+static TaskHandle_t imuTaskHandle = NULL; // Task handle for imu task
+
 // Function declarations
 static void ledBlink_task(void *pvParameters);
-static void uartRead_task(void *pvParameters);
+static void lidarRead_task(void *pvParameters);
 static void imuRead_task(void *imu_handle);
 
 
@@ -84,19 +85,19 @@ void app_main() {
         .device_address = 0x69,
         .scl_speed_hz = 100000,
     };
-    i2c_master_dev_handle_t imu_handle;
-    ESP_ERROR_CHECK(i2c_master_bus_add_device(bus_handle, &dev_cfg, &imu_handle));
+    i2c_master_dev_handle_t imu_device_handle;
+    ESP_ERROR_CHECK(i2c_master_bus_add_device(bus_handle, &dev_cfg, &imu_device_handle));
 
     // Initialise icm20948
-    imuBank = imu_init(imu_handle, imuBank, ACC_SCALE, GYRO_SCALE, OFFS_VALS);
+    imuBank = imu_init(imu_device_handle, imuBank, ACC_SCALE, GYRO_SCALE, OFFS_VALS);
 
     // LED
     gpio_set_direction(LED,GPIO_MODE_OUTPUT); // Setup GPIO
     
     // Tasks
     xTaskCreate(ledBlink_task, "ledBlink", 2048, NULL, 3, NULL);
-    xTaskCreate(uartRead_task, "uartRead", 2048, NULL, 10, NULL);
-    xTaskCreate(imuRead_task,"imuRead",2048,imu_handle,9,NULL);
+    xTaskCreate(lidarRead_task, "uartRead", 2048, NULL, 10, NULL);
+    xTaskCreate(imuRead_task,"imuRead",2048,imu_device_handle,9,&imuTaskHandle);
 
 }
 
@@ -117,20 +118,17 @@ static void ledBlink_task(void *pvParameters)
         printf("Time in LED task has been %.4f seconds!\n",(float)xTaskDetails.ulRunTimeCounter/1000);
         vTaskDelay(500/portTICK_PERIOD_MS);
     }
-    vTaskDelete(ledBlink_task);
 }
 
 // Task to read uart from lidar
-static void uartRead_task(void *pvParameters)
+static void lidarRead_task(void *pvParameters)
 {
     uart_event_t event;
     uint8_t* lidarBuffer = (uint8_t*) malloc(LIDAR_BUF_SIZE); // Buffer for lidar data
-    imu_queue = xQueueCreate(10,sizeof(uint8_t)); // Queue can hold 10 uint8s
-    uint8_t message;
 
     for(;;)
     {
-        xQueueReceive(uart2_queue, (void *)&event, (TickType_t)portMAX_DELAY); // Blocks indefinately
+        xQueueReceive(uart2_queue, (void *)&event, portMAX_DELAY); // Blocks indefinately
         if (!event.timeout_flag)
         {
             printf("Warning, lidar data not sent by timeout as expected...");
@@ -141,13 +139,8 @@ static void uartRead_task(void *pvParameters)
             uart_read_bytes(UART_PORT_NO, lidarBuffer, event.size ,portMAX_DELAY);
             printf("Distance = %d mm\n",lidarBuffer[3]<<8 | lidarBuffer[2]);
 
-            // Push frame header to lidar queue, don't wait.
-            message = lidarBuffer[0];
-            if( xQueueSendToBack(imu_queue,(void*) &message,0) != pdPASS )
-            {
-                // If failed to add
-                printf("Queue send to imu failed! There are %i spaces available", (int) uxQueueSpacesAvailable(imu_queue));
-            }
+            // Unblock imu task if it exists yet
+            if(imuTaskHandle != NULL) xTaskNotifyGive(imuTaskHandle);
         }
         else
         {
@@ -155,29 +148,21 @@ static void uartRead_task(void *pvParameters)
 
         }
     }
-    vTaskDelete(uartRead_task);
 }
 
 // Task to read imu
-static void imuRead_task(void *imu_handle)
+static void imuRead_task(void *imu_device_handle)
 {
     uint8_t imuBuffer[IMU_BUF_SIZE];
-    uint8_t message;
-    imu_queue = xQueueCreate(10,sizeof(uint8_t)); // Queue can hold 10 uint8s
 
     for(;;)
     {
-        xQueueReceive(imu_queue, (void *)&message, (TickType_t)portMAX_DELAY); // Wait indefinately
+        ulTaskNotifyTake(pdTRUE,portMAX_DELAY); // Block until notified by lidar task
 
-        // We expect to recieve 0x59 from lidar task
-        if( message == 0x59 )
-        {
-            // Read accel, gyro and external sensor (mag) registers
-            imuBank = imu_read(imu_handle, imuBank, ACCEL_XOUT_H, imuBuffer, 22);
-            printf("IMU value is %i\n",imuBuffer[18]);
-        }
-
-        vTaskDelete(imuRead_task);
+        // Read accel, gyro and external sensor (mag) registers
+        imuBank = imu_read(imu_device_handle, imuBank, ACCEL_XOUT_H, imuBuffer, 22);
+        printf("IMU value is %i\n",imuBuffer[18]);
         
     }
+    vTaskDelete(imuTaskHandle);
 }
